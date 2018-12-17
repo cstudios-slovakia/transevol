9aa23230fa21226aa264d3c614e8d817117c5663
lostika <eugen.juhos@gmail.com>
2018-12-17 16:33:51 +0100

    dektrium / admin creates user change Task #4963

--- controllers/user/AdminController.php
+++ controllers/user/AdminController.php
@@ -7,4 +7,27 @@ use dektrium\user\controllers\AdminController as BaseAdminController;
 class AdminController extends BaseAdminController
 {
     use LoggedInUserTrait;
+
+    /** @inheritdoc */
+    public function behaviors()
+    {
+        $behaviors    = parent::behaviors();
+
+        $accessRules = [
+            [
+                'allow' => true,
+                'actions' => ['switch'],
+                'roles' => ['@'],
+            ],
+            [
+                'allow' => true,
+                'roles' => ['companyAdmin'],
+            ],
+        ];
+
+        $behaviors['access']['rules'] = $accessRules;
+
+        return $behaviors;
+
+    }
 }
--- views/user/admin/create.php
+++ views/user/admin/create.php
@@ -12,7 +12,7 @@
 use yii\bootstrap\ActiveForm;
 use yii\bootstrap\Nav;
 use yii\helpers\Html;
-
+use yii\helpers\Url;
 /**
  * @var yii\web\View $this
  * @var dektrium\user\models\User $user
@@ -25,59 +25,70 @@ $this->params['breadcrumbs'][] = $this->title;
 
 <?= $this->render('/_alert', ['module' => Yii::$app->getModule('user'),]) ?>
 
-<?= $this->render('_menu') ?>
+
+<?php $this->beginContent('@app/views/layouts/default/common/pages/create.php'); ?>
+
+<div class="m_portlet-body">
+
 
 <div class="row">
     <div class="col-md-3">
-        <div class="panel panel-default">
-            <div class="panel-body">
-                <?= Nav::widget([
-                    'options' => [
-                        'class' => 'nav-pills nav-stacked',
-                    ],
-                    'items' => [
-                        ['label' => Yii::t('user', 'Account details'), 'url' => ['/user/admin/create']],
-                        ['label' => Yii::t('user', 'Profile details'), 'options' => [
-                            'class' => 'disabled',
-                            'onclick' => 'return false;',
-                        ]],
-                        ['label' => Yii::t('user', 'Information'), 'options' => [
-                            'class' => 'disabled',
-                            'onclick' => 'return false;',
-                        ]],
-                    ],
-                ]) ?>
-            </div>
+        <div class="m-demo__preview">
+            <ul class="m-nav">
+                <li class="m-nav__item m-nav__item--active">
+                    <a href="<?= Url::toRoute('/user/admin/create') ?>" class="m-nav__link">
+                        <i class="m-nav__link-icon flaticon-plus"></i>
+                        <span class="m-nav__link-text"><?= Yii::t('admin/user','Add new company user') ?></span>
+                    </a>
+                </li>
+                <li class="m-nav__item m-nav__item--disabled" onclick="return false;">
+                    <a href="#" class="m-nav__link" disabled="disabled">
+                        <i class="m-nav__link-icon flaticon-profile-1"></i>
+                        <span class="m-nav__link-text"><?= Yii::t('user', 'Profile details') ?></span>
+                    </a>
+                </li>
+                <li class="m-nav__item m-nav__item--disabled"  onclick="return false;">
+                    <a href="#" class="m-nav__link"  disabled="disabled">
+                        <i class="m-nav__link-icon flaticon-info"></i>
+                        <span class="m-nav__link-text"><?= Yii::t('user', 'Information') ?></span>
+                    </a>
+                </li>
+
+            </ul>
         </div>
     </div>
     <div class="col-md-9">
+        <?php $form = \app\components\ViewTyped\Form\BaseCreateFormWidget::begin([
+//    'layout' => 'horizontal',
+            'enableAjaxValidation' => true,
+            'enableClientValidation' => false,
+//    'fieldConfig' => [
+//        'horizontalCssClasses' => [
+//            'wrapper' => 'col-sm-9',
+//        ],
+//    ],
+        ]); ?>
+
+
         <div class="panel panel-default">
             <div class="panel-body">
                 <div class="alert alert-info">
                     <?= Yii::t('user', 'Credentials will be sent to the user by email') ?>.
                     <?= Yii::t('user', 'A password will be generated automatically if not provided') ?>.
                 </div>
-                <?php $form = ActiveForm::begin([
-                    'layout' => 'horizontal',
-                    'enableAjaxValidation' => true,
-                    'enableClientValidation' => false,
-                    'fieldConfig' => [
-                        'horizontalCssClasses' => [
-                            'wrapper' => 'col-sm-9',
-                        ],
-                    ],
-                ]); ?>
 
-                <?= $this->render('_user', ['form' => $form, 'user' => $user]) ?>
-
-                <div class="form-group">
-                    <div class="col-lg-offset-3 col-lg-9">
-                        <?= Html::submitButton(Yii::t('user', 'Save'), ['class' => 'btn btn-block btn-success']) ?>
-                    </div>
-                </div>
-
-                <?php ActiveForm::end(); ?>
             </div>
         </div>
+        <?= $this->render('_user', ['form' => $form, 'user' => $user]) ?>
+
+        <?php \app\components\ViewTyped\Form\BaseCreateFormWidget::end() ?>
     </div>
 </div>
+
+</div>
+
+
+
+
+
+<?php $this->endContent(); ?>
