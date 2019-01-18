5fcfa8c0d1494e7206692bed8bea9b2566feef7b
lostika <eugen.juhos@gmail.com>
2019-01-18 15:51:41 +0100

    redesign transport database into new idea Task #5035 - in Progress

---
+++ migrations/m190118_134648_TransporterTable.php
@@ -0,0 +1,44 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m190118_134648_TransporterTable
+ */
+class m190118_134648_TransporterTable extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+
+
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m190118_134648_TransporterTable cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m190118_134648_TransporterTable cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
